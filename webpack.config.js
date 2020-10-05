71cb0019cd97b61473eeb3845400b428a146ad35
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-10-05 12:38:51 +0900

    chore: Create webpack.config.js file
    Add npm "dev" script

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+// Is this line necessary?
+//const webpack = require('webpack')
+
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.png$|\.jpg$/,
+                loader: 'file-loader',
+                options: { name: '[name].[ext]' }
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: false,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+    ]
+}
