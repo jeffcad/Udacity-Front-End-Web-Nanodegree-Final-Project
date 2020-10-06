a9e1131228ae311131c05c93747f5a51a9d412ee
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-10-06 16:29:24 +0900

    chore: Add production mode
    Add dist to .gitignore, create "build" script in package.json, split
    webpack.config.js into webpack.dev.js and webpack.prod.js

---
+++ webpack.prod.js
@@ -0,0 +1,55 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'production',
+    // output: {
+    //     libraryTarget: 'var',
+    //     library: 'Client'
+    // },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.png$|\.jpg$/,
+                loader: 'file-loader',
+                options: { name: '[name].[ext]' }
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: false,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
+    ],
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+}
