93e5ddc403f569ea7c1ad1c488d5fc8fd822fba4
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-10-06 13:00:56 +0900

    code: Installed and introduced npm Countdown
    Use countdown to get time from now until trip departure date

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,13 +1,21 @@
+const countdown = require('countdown')
+
 export function submitted(event) {
     event.preventDefault()
-
     console.log('Event listener connected')
+
     const destinationCity = document.getElementById('destination-city').value
     console.log(`City: ${destinationCity}`)
+
     const departureDate = document.getElementById('departure-date').value
     console.log(`Departure date: ${departureDate}`)
-    const userData = { "destination": destinationCity, "departure": departureDate }
+
+    const timeUntilTrip = countdown(null, new Date(departureDate), countdown.DAYS).days
+    console.log(`Days until departure: ${timeUntilTrip}`)
+
+    const userData = { "destination": destinationCity, "departure": departureDate, "countdown": timeUntilTrip }
     console.log(userData)
+
     apiCalls(userData)
 }
 
