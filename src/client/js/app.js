7efb71cc6d22655d91805e157deca34f8920315b
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-10-07 15:05:41 +0900

    feat: Add return date and units selector

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -9,12 +9,24 @@ export async function submitted(event) {
 
     const departureDate = document.getElementById('departure-date').value
     console.log(`Departure date: ${departureDate}`)
+    const returnDate = document.getElementById('return-date').value
+    console.log(`Return date: ${returnDate}`)
 
     const timeUntilTrip = countdown(null, new Date(departureDate), countdown.DAYS).days
     console.log(`Days until departure: ${timeUntilTrip}`)
+    const timeUntilReturn = countdown(null, new Date(returnDate), countdown.DAYS).days
+    const tripDuration = timeUntilReturn - timeUntilTrip
+
+    const unitsInput = document.querySelector('input[name="units"]:checked').value
+    let units
+    if (unitsInput == "imperial") {
+        units = "I"
+    } else {
+        units = "M"
+    }
 
     let bigData = {}
-    bigData["userData"] = { destinationCity, departureDate, timeUntilTrip }
+    bigData["userData"] = { destinationCity, departureDate, timeUntilTrip, timeUntilReturn, tripDuration, units }
     console.log(bigData.userData)
 
     bigData = await apiCalls(bigData)
@@ -27,8 +39,8 @@ async function apiCalls(bigData) {
     bigData["cityData"] = extractCityData(geonameData)
     console.log(bigData.cityData)
 
-    const weatherbitData = await getWeatherbitData(bigData.cityData)
-    bigData["forecastData"] = extractForecastData(weatherbitData, bigData.userData.timeUntilTrip)
+    const weatherbitData = await getWeatherbitData(bigData.cityData, bigData.userData.units)
+    bigData["forecastData"] = extractForecastData(weatherbitData, bigData.userData.timeUntilTrip, bigData.userData.tripDuration)
     console.log(bigData.forecastData)
 
     const photoData = await getPhotoData(bigData.userData.destinationCity)
@@ -66,7 +78,7 @@ function extractCityData(geonameData) {
     return { latitude, longitude, country, population }
 }
 
-async function getWeatherbitData(cityData) {
+async function getWeatherbitData(cityData, units) {
     const response = await fetch('http://localhost:8081/callweather', {
         method: 'POST',
         credentials: 'same-origin',
@@ -74,18 +86,22 @@ async function getWeatherbitData(cityData) {
             'Content-Type': 'application/json'
         },
         // Body data type must match "Content-Type" header        
-        body: JSON.stringify(cityData)
+        body: JSON.stringify({ cityData, units })
     })
 
     const responseJSON = await response.json()
     return responseJSON
 }
 
-function extractForecastData(weatherbitData, timeUntilTrip) {
+function extractForecastData(weatherbitData, timeUntilTrip, tripDuration) {
     const forecastData = []
 
     // counter max is 15 because API currently returns max 16 days data
-    for (let i = timeUntilTrip; i <= 15; i++) {
+    let lastForecastDay = 15
+    if ((timeUntilTrip + tripDuration) < 15) {
+        lastForecastDay = timeUntilTrip + tripDuration
+    }
+    for (let i = timeUntilTrip; i <= lastForecastDay; i++) {
         const date = weatherbitData.data[i].valid_date
         const windSpeed = weatherbitData.data[i].wind_spd
         const windDirection = weatherbitData.data[i].wind_dir
@@ -154,11 +170,11 @@ async function storeBigData(bigData) {
 
 function updateUI(bigData) {
 
-    let days = "days"
+    let daysString = "days"
     if (bigData.userData.timeUntilTrip == 1) {
-        days = "day"
+        daysString = "day"
     }
-    document.getElementById('countdown').innerHTML = `Your trip to ${bigData.userData.destinationCity} is coming up in ${bigData.userData.timeUntilTrip} ${days}!`
+    document.getElementById('countdown').innerHTML = `Your trip to ${bigData.userData.destinationCity} is coming up in ${bigData.userData.timeUntilTrip} ${daysString}!`
     document.getElementById('forecast-title').innerHTML = "Here is the forecast for your trip:"
     const locationImage = document.createElement('img')
     locationImage.src = bigData.photo
@@ -171,20 +187,28 @@ function updateUI(bigData) {
     const fragment = document.createDocumentFragment()
     const forecasts = bigData.forecastData
     for (const forecast of forecasts) {
-        const forecastCard = createForecastCard(forecast)
+        const forecastCard = createForecastCard(forecast, bigData.userData.units)
         fragment.append(forecastCard)
     }
     const forecastCardContainer = document.getElementById('forecast-card-container')
+    forecastCardContainer.innerHTML = ""
     forecastCardContainer.append(fragment)
 
 }
 
-function createForecastCard(forecast) {
+function createForecastCard(forecast, units) {
+    let temperatureUnit = "C"
+    let speedUnit = "m/s"
+    let depthUnit = "mm"
+    if (units == "I") {
+        temperatureUnit = "F"
+        speedUnit = "mph"
+        depthUnit = "in"
+    }
 
     const forecastCard = document.createElement('div')
     forecastCard.classList.add('forecast-card')
 
-    //Can I clean this all up into an iteration? Spread operator?
     const dateDiv = document.createElement('div')
     dateDiv.classList.add('date')
     dateDiv.innerHTML = forecast.date
@@ -199,11 +223,11 @@ function createForecastCard(forecast) {
 
     const highTemperatureDiv = document.createElement('div')
     highTemperatureDiv.classList.add('high-temperature')
-    highTemperatureDiv.innerHTML = `High temperature: ${forecast.highTemperature}\xB0C`
+    highTemperatureDiv.innerHTML = `High temperature: ${forecast.highTemperature}\xB0${temperatureUnit}`
 
     const lowTemperatureDiv = document.createElement('div')
     lowTemperatureDiv.classList.add('low-temperature')
-    lowTemperatureDiv.innerHTML = `Low temperature: ${forecast.lowTemperature}\xB0C`
+    lowTemperatureDiv.innerHTML = `Low temperature: ${forecast.lowTemperature}\xB0${temperatureUnit}`
 
     const humidityDiv = document.createElement('div')
     humidityDiv.classList.add('humidity')
@@ -215,15 +239,15 @@ function createForecastCard(forecast) {
 
     const precipitationDiv = document.createElement('div')
     precipitationDiv.classList.add('precipitation')
-    precipitationDiv.innerHTML = `Precipitation amount: ${forecast.precipitation.toFixed(1)}mm`
+    precipitationDiv.innerHTML = `Precipitation amount: ${forecast.precipitation.toFixed(1)}${depthUnit}`
 
     const snowDiv = document.createElement('div')
     snowDiv.classList.add('snow')
-    snowDiv.innerHTML = `Snow amount: ${forecast.snow.toFixed(1)}mm`
+    snowDiv.innerHTML = `Snow amount: ${forecast.snow.toFixed(1)}${depthUnit}`
 
     const windSpeedDiv = document.createElement('div')
     windSpeedDiv.classList.add('wind-speed')
-    windSpeedDiv.innerHTML = `Windspeed: ${forecast.windSpeed.toFixed(1)}m/s`
+    windSpeedDiv.innerHTML = `Windspeed: ${forecast.windSpeed.toFixed(1)}${speedUnit}`
 
     const windDirectionDiv = document.createElement('div')
     windDirectionDiv.classList.add('wind-direction')
--- src/server/server.js
+++ src/server/server.js
@@ -57,12 +57,12 @@ async function callGeo(req, res) {
 app.post('/callweather', callWeather)
 
 async function callWeather(req, res) {
-    console.log(`Request latitude is ${req.body.latitude}`)
-    console.log(`Request longitude is ${req.body.longitude}`)
-    const latitude = req.body.latitude
-    const longitude = req.body.longitude
+    console.log(`Request latitude is ${req.body.cityData.latitude}`)
+    console.log(`Request longitude is ${req.body.cityData.longitude}`)
+    const latitude = req.body.cityData.latitude
+    const longitude = req.body.cityData.longitude
     const locationURL = `lat=${latitude}&lon=${longitude}`
-    const units = "M"
+    const units = req.body.units
     const weatherbitURL = WEATHERBIT_ROOT + locationURL + WEATHERBIT_KEY_URL + WEATHERBIT_PARAMS + units
     console.log(`Weatherbit URL is ${weatherbitURL}`)
     const response = await fetch(weatherbitURL)
